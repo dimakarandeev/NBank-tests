9c8bd5fc5578a98fe9a3d15672f92a21ca8e3c58
Dima Karandeev <karandeeff.dima@yandex.ru>
2026-07-18 12:46:54 +0300

    - Добывлены логи

--- infa/docker_compose/run-tests-with-docker-compose.sh
+++ infa/docker_compose/run-tests-with-docker-compose.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 IMAGE_NAME="${IMAGE_NAME:-nbank-tests}"
+TIMESTAMP=$(date +"%Y%m%d_%H%M")
+TEST_OUTPUT_DIR=$(pwd -W)/test-output/$TIMESTAMP
 
 echo "Останавливаем тестовое окружение"
 docker compose down
@@ -18,19 +20,34 @@ else
     echo "Образ $IMAGE_NAME уже существует"
 fi
 
+mkdir -p "$TEST_OUTPUT_DIR/logs"
+mkdir -p "$TEST_OUTPUT_DIR/results"
+mkdir -p "$TEST_OUTPUT_DIR/report"
 
 echo "Поднимаем тестовое окружение с помощью Docker Compose"
 docker compose up -d --remove-orphans
 echo "Окружение запущено"
 
+echo "⏳ Ждём 5 секунд для запуска сервисов..."
+sleep 5
+
 echo ">>> Запустить контейнер с тестами"
 docker run --rm \
+  --network nbank-network \
+  -v "$TEST_OUTPUT_DIR/logs":/app/logs \
+  -v "$TEST_OUTPUT_DIR/results":/app/target/surefire-reports \
+  -v "$TEST_OUTPUT_DIR/report":/app/target/site \
   -e APIBASEURL=http://localhost:4111 \
-  -e  UIBASEURL=http://localhost:3000 \
-  -e  SELENOID_URL=http://localhost:4444 \
+  -e UIBASEURL=http://localhost:3000 \
+  -e SELENOID_URL=http://localhost:4444 \
   -e SELENOID_UI_URL=http://localhost:8080 \
   -e TEST_PROFILE="api,ui" \
   "$IMAGE_NAME"
+
+echo ">>> Тесты завершены"
+echo "Лог файл: $TEST_OUTPUT_DIR/logs/run.log"
+echo "Результаты тестов: $TEST_OUTPUT_DIR/results"
+echo "Репорт: $TEST_OUTPUT_DIR/report"
 
 echo ">>> Остановка окружения"
 docker compose down
